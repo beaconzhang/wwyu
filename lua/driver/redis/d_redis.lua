23958c5544b26de2bf1b3a01c8947bcdde3cdcd3
beaconzhang <xzhangwwyu@gmail.com>
2018-06-29 09:04:55 +0800

    feat add redis driver

--- lua/driver/redis/d_redis.lua
+++ lua/driver/redis/d_redis.lua
@@ -3,11 +3,49 @@ local global = require("./common/global")
 local lock = require("resty.lock")
 local util = require("./common/util")
 
-redis_client = nil
+local _M = {}
 
-function get_client()
-	local lock, err = resty_lock:new("d_redis")
-	if not lock then
-		util.log_error("lock d_redis errir
-	
+function _M.get_client()
+	local redis_client = redis:new()
+	local ok, err = redis_client:connect(g_lua_conf.redis_host, g_lua_conf.redis_port)
+	if not ok then
+		util.log_error("get redis client failed error redis_host:"..g_lua_conf.redis_host .. " redis_port:"..g_lua_conf.redis_port.." error message:"..err)
+	end
+	ok, err = red:set_keepalive(g_lua_conf.max_idle_timeout,g_lua_conf.pool_size)
+	return redis_client
 end
+
+function redis_set(key,value)
+	local redis_client = _M.get_client()
+	local ok, err = redis_client:set(key,value)
+	if not ok then
+		util.log_error("redis set error key:" .. key .. " value:" .. value .. " error:"..err)
+	end
+	return err
+end
+
+
+function redis_get(key)
+	local redis_client = _M.get_client()
+	local ok, err = redis_client:get(key)
+	if not ok then
+		util.log_error("redis get error key:" .. key .. " error:"..err)
+	end
+	return ok
+end
+
+function redis_expire(key,expire_time)
+	local redis_client = _M.get_client()
+	local ok,err = redis_client:expire(key,expire_time)
+	if not ok then
+		util.log_error("redis expire error key:" .. key .. "expire_time:" .. expire_time .. " error:"..err)
+	end
+	return ok
+end
+
+--参数格式为{{key,value},{key,value}, ... }
+function redis_set_pipe(argv)
+	local redis_client = _M.get_client()
+	local 
+end
+
